be7e55c85c36252cd6d4970172835b677ee4bf26
Azula21 <janelinw@usc.edu>
2018-07-20 14:41:03 -0700

    Many changes

--- js/real.js
+++ js/real.js
@@ -1,6 +1,8 @@
+
 $(document).ready(function(){
     $(".button1").click(function(){
-    	$(".hacker, .engineer, .student").hide("slow")
+    	$(".hacker, .engineer, .student").hide("slow");
+        change_artist();
         $(".artist").toggle("slow");
     });
     $(".button1").hover(function(){
@@ -28,6 +30,7 @@ $(document).ready(function(){
 	});
 	$(".button4").click(function(){
 		$(".artist, .student, .hacker").hide("slow")
+        change_engineer();
         $(".engineer").toggle("slow");
     });
     $(".button4").hover(function(){
@@ -36,3 +39,10 @@ $(document).ready(function(){
     	$(this).css("color", "#000000");
 	});
 });
+
+function change_artist(){
+  window.location = "artist.html";
+}
+function change_engineer(){
+  window.location = "engineer.html";
+}
